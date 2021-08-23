392ee52a0e7f3cefca74c509717bd6f368492ce9
bjl0615 <bjl0615@gmail.com>
2021-08-23 21:57:07 +0900

    js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const calculator = {
+    add : function(a , b){
+        console.log(a+b);
+    },
+    minus : function(a , b){
+        console.log(a - b);
+    },
+    multiply : function(a , b){
+        console.log(a * b);
+    },
+    division : function(a,b){
+        console.log(a / b);
+    }
+
+}
+
+calculator.add(1,2);
