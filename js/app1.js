1d19f700bec2d0546f96c60bf28781476e130b56
bjl0615 <bjl0615@gmail.com>
2021-08-26 22:58:51 +0900

    add resize, copy, offline, online

--- js/app1.js
+++ js/app1.js
@@ -1,19 +1,41 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+console.dir(h1);
 
 function handleTitleClick(){
-    title.style.color="blue";
+    h1.style.color="blue";
 }
 
 function handleMouseEnter(){
-   title.innerText = "Mouse is here!";
+   h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handlerWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handlerWindowCopy(){
+    alert("copier!");
+}
+
+function handlerWindowOffListener(){
+    alert("SOS no WIFI");
+}
+
+function handleWindoOnLine(){
+    alert("ALL GOOD")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handlerWindowResize);  
+window.addEventListener("copy", handlerWindowCopy);
+window.addEventListener("offline", handlerWindowOffListener);
+window.addEventListener("online", handleWindoOnLine)
