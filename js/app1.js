c2b60ef40f0ce705ec4efadbd9f7efb8be511e51
bjl0615 <bjl0615@gmail.com>
2021-08-27 22:19:28 +0900

    add if,else

--- js/app1.js
+++ js/app1.js
@@ -3,39 +3,46 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleTitleClick(){
-    h1.style.color="blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else {
+        newColor = "blue";
+    }   
+    h1.style.color = newColor; 
 }
-
-function handleMouseEnter(){
-   h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handlerWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handlerWindowCopy(){
-    alert("copier!");
-}
-
-function handlerWindowOffListener(){
-    alert("SOS no WIFI");
-}
-
-function handleWindoOnLine(){
-    alert("ALL GOOD")
-}
-
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
+
+// function handleMouseEnter(){
+//    h1.innerText = "Mouse is here!";
+// }
+
+// function handleMouseLeave(){
+//     h1.innerText = "Mouse is gone!";
+// }
+
+// function handlerWindowResize(){
+//     document.body.style.backgroundColor = "tomato";
+// }
+
+// function handlerWindowCopy(){
+//     alert("copier!");
+// }
+
+// function handlerWindowOffListener(){
+//     alert("SOS no WIFI");
+// }
+
+// function handleWindoOnLine(){
+//     alert("ALL GOOD")
+// }
+
+// h1.addEventListener("mouseenter", handleMouseEnter);
+// h1.addEventListener("mouseleave", handleMouseLeave);
 
 
-window.addEventListener("resize", handlerWindowResize);  
-window.addEventListener("copy", handlerWindowCopy);
-window.addEventListener("offline", handlerWindowOffListener);
-window.addEventListener("online", handleWindoOnLine)
+// window.addEventListener("resize", handlerWindowResize);  
+// window.addEventListener("copy", handlerWindowCopy);
+// window.addEventListener("offline", handlerWindowOffListener);
+// window.addEventListener("online", handleWindoOnLine)
