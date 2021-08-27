f883dd6d151d81e091e9134becb589756ed20d8f
bjl0615 <bjl0615@gmail.com>
2021-08-27 22:48:08 +0900

    js

--- js/app1.js
+++ js/app1.js
@@ -3,10 +3,6 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleTitleClick(){
-    if(h1.className === "active"){
-        h1.className = "";
-    }else {
-        h1.className = "active";
-    }
+    h1.classList.toggle("clicked");
 }
 h1.addEventListener("click", handleTitleClick);
