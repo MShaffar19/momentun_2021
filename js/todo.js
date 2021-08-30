c8c5db92bf1fca25ce5a6e46bd91623204368c9b
bjl0615 <bjl0615@gmail.com>
2021-08-30 23:07:11 +0900

    todo

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -12,7 +20,7 @@ function paingToDO(newTodo){
     const span = document.createElement("span");
     span.innerText = newTodo;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.innerText = "✖";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
@@ -23,7 +31,16 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paingToDO(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is a the turn of ", item));
+}
